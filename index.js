c03b546b32315e70a654582aba812b98343ce280
Apple <apple@Apples-MacBook-Pro.local>
2025-08-16 00:17:39 +0500

    Updated index.js with new functionality

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let count = 0;
 
+if(localStorage.getItem('counter')){
+    count = parseInt(localStorage.getItem('counter'))
+}
+document.getElementById('counter').textContent= count;
+
 const increase = document.getElementById('increase');
 const decrease = document.getElementById('decrease')
 const reset = document.getElementById('reset')
@@ -7,18 +12,18 @@ const reset = document.getElementById('reset')
 
 increase.addEventListener('click', function(){
     count = count+1;
-    let val = document.getElementById('counter')
-    val.textContent=count;
+     document.getElementById('counter').textContent = count;
+        localStorage.setItem('counter', count);
 })
 
 decrease.addEventListener('click', function(){
     count = count-1;
-     let val = document.getElementById('counter')
-    val.textContent=count;
+     document.getElementById('counter').textContent = count;
+        localStorage.setItem('counter', count);
 })
 
 reset.addEventListener('click', function(){
-    count = 0;
-     let val = document.getElementById('counter')
-    val.textContent=count;
+    count = 0;  
+    document.getElementById('counter').textContent = count;
+        localStorage.setItem('counter', count);
 })
